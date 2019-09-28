8ba4b24dbd680a70d58b91cf4d4b969fdf1b0efa
austinkschmitz <35646306+austinkschmitz@users.noreply.github.com>
2019-09-27 18:49:11 -0600

    Update Discord.py

--- Discord.py
+++ Discord.py
@@ -2,26 +2,27 @@
 # https://discordpy.readthedocs.io/en/latest/api.html#discord.Permissions.manage_roles
 # https://discordpy.readthedocs.io/en/latest/api.html#discord.Guild.get_member
 
+# TODO:
+#   Message Everyone that varuser checkedin in new channel
+#   Combine Barcode to discord
+
 import csv
 import discord
 from discord.ext import commands
 
-TOKEN = 'str'
+TOKEN = ''
 
 
 def pull_csv(number_query):
     with open('datatest.csv') as csvfile:
-        readCSV = csv.reader(csvfile, delimiter=',')
-        CardNumbers = []
-        ScreenNames = []
-        RealNames = []
-        for row in readCSV:
-            CardNumber = row[0]
-            RealName = row[1]
-            ScreenName = row[2]
-
-            if number_query == int(CardNumber):
-                this_tuple = (RealName, ScreenName, CardNumber)
+        read_csv = csv.reader(csvfile, delimiter=',')
+        for row in read_csv:
+            card_number = row[0]
+            real_name = row[1]
+            screen_name = row[2]
+            seat_number = row[3]
+            if number_query == int(card_number):
+                this_tuple = (card_number, real_name, screen_name, seat_number)
                 return this_tuple
 
 
@@ -43,9 +44,6 @@ class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
-
 
 @client.event
 async def on_ready():
@@ -60,17 +58,28 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello! ' + str(message.author))
 
-    if message.content.startswith('!!'):
-        x = message.guild.members
-        for member in x:
+
+    if message.content.startswith('0'):
+        server = message.guild.members
+        guild = message.guild
+        Bar_code = int(message.content)
+        New_Name = (pull_csv(Bar_code))
+        screen_name = New_Name[2]
+        seat_number = New_Name[3]
+        for member in server:
             if member.name == screen_name:
                 print(str(seat_number) + ':' + member.name)
                 print(member.roles)
                 print(member.guild_permissions)
                 print(member.nick)
-                await member.edit(nick='boom')
+                await member.edit(nick=(seat_number + ":" + str(screen_name)))
                 role = discord.utils.get(member.guild.roles, name="moved")
                 await member.add_roles(role)
+                await message.delete()
+                await message.channel.send('@here  Welcome to the LAN ' + str(screen_name) + "!")
+
+
 
 
 client.run(TOKEN)
+
